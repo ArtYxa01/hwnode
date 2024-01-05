e5a2d45c036b8a285ee54fa6171458a164546707
ArtYxa01 <115730030+ArtYxa01@users.noreply.github.com>
2024-01-05 21:59:47 +0200

    gtrgrthrththtrhbtgrfd

--- models/contacts.js
+++ models/contacts.js
@@ -1,39 +1,18 @@
-const { Schema, model } = require('mongoose'); // Підключення модулів Schema та model з mongoose
-const Joi = require('joi'); // Підключення модуля Joi для валідації даних
-const { handleMongooseError } = require('../helpers'); // Підключення функції handleMongooseError з папки '../helpers'
+const {Contact, addSchema, updateFavoriteSchema} = require ('../models/contacts')
+const { HttpError }= require('../helpers')
+const ctrlWrapper = require('../helpers/ctrlWrapper')
+const listContacts = async (req, res, next) => {
+  const result = await Contact.find()
+  res.json(result)
+  console.log(result)
+}
 
-const contactSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Set name for contact'], // Властивість name має тип String та обов'язкова для заповнення. Якщо значення не вказане, генерується помилка з повідомленням "Set name for contact"
-    },
-    email: {
-      type: String,
-    },
-    phone: {
-      type: String,
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { versionKey: false }, 
-);
+const getById = async (req, res, next) => {
+  const { id } = req.params
+  const result = await Contact.findById(id)
 
-contactSchema.post('save', handleMongooseError); 
-
-const Contact = model('contact', contactSchema); 
-
-const addSchema = Joi.object({
-  name: Joi.string().required(), 
-  email: Joi.string().required(), 
-  phone: Joi.string().required(), 
-});
-
-const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required(), 
-});
-
-module.exports = { Contact, addSchema, updateFavoriteSchema }; 
+  if (!result) {
+    throw HttpError(404, 'notFound')
+  }
+  res.json(result)
+}
